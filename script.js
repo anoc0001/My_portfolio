5957b8062a972fbb0bb0a4c3cd91b0d42c64d315
anoc0001 <anochiriashley@gmail.com>
2026-03-22 22:33:37 -0400

    adding new images, html,css for the flower shop project

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ setInterval(changeWord, 2000);
 //optional click to change.
 document.addEventListener('click', changeWord);
 // endss....
-
-/*===== MENU SHOW Y HIDDEN =====*/
+/* ==
+/*===== MENU SHOW Y HIDDEN =====*
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
@@ -34,7 +34,9 @@ closeMenu.addEventListener('click', ()=>{
   navMenu.classList.remove('show')
 })
 
-/*===== MOUSEMOVE HOME IMG =====*/
+== */
+
+/*===== MOUSEMOVE HOME IMG =====
 document.addEventListener('mousemove', move);
 function move(e){
   this.querySelectorAll('.move').forEach(layer =>{
@@ -46,17 +48,7 @@ function move(e){
       layer.style.transform = `translateX(${x}px) translateY(${y}px)`
   })
 }
-
-/*===== GSAP ANIMATION =====*/
-// NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
-
-// HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+== */
 
 
 
